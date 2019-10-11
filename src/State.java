0ad255d1a533ecc2b617b00da655c33c3b274f92
he-eric <eriche864@gmail.com>
2019-10-11 13:41:11 -0400

    Adaptive

---
+++ src/State.java
@@ -0,0 +1,40 @@
+
+public class State {
+	
+	int gValue;
+	int hValue;
+	int fValue;
+	int x, y;
+	int iteration = Integer.MIN_VALUE;
+	byte pointer;
+	
+	public State(int x, int y, int gValue, int hValue) {
+		this.x = x;
+		this.y = y;
+		this.gValue = gValue;
+		this.hValue = hValue;
+	}
+	
+	public State(int x, int y, int hValue) {
+		this(x, y, 0, hValue);
+	}
+	
+	public String toString(){
+		return "[x: " + x + " y: "+ y + "] with f: " + fValue + " with g: " + gValue;
+	}
+	
+	@Override
+	public boolean equals(Object c) {
+		if(this == null || c == null)
+			return false;
+		if (this.getClass() != c.getClass()) 
+			return false;
+		State cc = (State) c;
+					
+		if(this.x == cc.x && this.y == cc.y){
+			return true;
+		}
+		return false;
+	}
+	
+}
